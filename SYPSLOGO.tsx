2da4a7489c4ea6a219b7bfcf7cb425c5dfdca83e
liao-chianan <lcn@syps.tp.edu.tw>
2022-05-24 11:23:56 +0800

    upload logo

---
+++ SYPSLOGO.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="SYPS" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+ <tileoffset x="1024" y="1024"/>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="32" height="32" source="logo-01.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="logo-02.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="logo-03.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="logo-04.png"/>
+ </tile>
+</tileset>
